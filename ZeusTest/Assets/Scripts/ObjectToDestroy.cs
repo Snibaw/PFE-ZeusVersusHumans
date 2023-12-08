515351555d6064353fc656b5d766ab325e0e0447
Snibaw <Snibaw@gmail.com>
2023-12-08 17:00:23 +0100

    No Health Bar for resources
    Health bars on resources doesn't fit well with the game right now

--- ZeusTest/Assets/Scripts/ObjectToDestroy.cs
+++ ZeusTest/Assets/Scripts/ObjectToDestroy.cs
@@ -21,8 +21,8 @@ public class ObjectToDestroy : MonoBehaviour
         // {
         //     healthBar.ActivateHealthBar();
         // }
-        healthBar.SetMaxHealth(life);
-        healthBar.DamageRessource(damage);
+        // healthBar.SetMaxHealth(life);
+        // healthBar.DamageRessource(damage);
 
         life -= damage;
 
