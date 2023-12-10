7cc42c5d146c1de57eaf4241f3a81c658e9cf7f3
Snibaw <Snibaw@gmail.com>
2023-12-10 15:38:00 +0100

    Adapt sphere to relief of planet

--- ZeusTest/Assets/Scripts/GraphNode.cs
+++ ZeusTest/Assets/Scripts/GraphNode.cs
@@ -13,15 +13,12 @@ public class GraphNode
     {
         RaycastHit hit;
         //Raycast from above the point to the center of the sphere (0,0,0)
-        if (Physics.Raycast(Position * 1.1f, (Vector3.zero - Position * 1.1f), out hit, 2f)) // 2f : experimental
+        if (Physics.Raycast(Position, (Vector3.zero - Position), out hit, Position.magnitude)) // 2f : experimental
         {
+            Position = hit.point;
             if (hit.collider.gameObject.CompareTag("Water"))
             {
                 IsObstacle = true;
-            }
-            else if (hit.collider.gameObject.CompareTag("Ground")) // Adapt the position to the ground
-            {
-                Position = hit.point;
             }
         }
         else // If the raycast doesn't hit anything, it's an obstacle
