5170103df48aa963bb38026a1327174aef88d82f
Isunia14 <94149685+Isunia14@users.noreply.github.com>
2024-01-24 15:11:44 +0100

    crown +outlined houses

--- ZeusTest/Assets/TownDevelopmentSlider.cs
+++ ZeusTest/Assets/TownDevelopmentSlider.cs
@@ -11,6 +11,7 @@ public class TownDevelopmentSlider : MonoBehaviour
     [SerializeField] private Image[] housesImages;
     [SerializeField] private Slider leftSlider;
     [SerializeField] private Slider middleSlider;
+    [SerializeField] private GameObject[] crownImages; // 0 is  left, 1 middle, 2 right
     int totalScore = 3;
     List<int> scores = new List<int>() { 1, 1, 1 };
     
@@ -31,6 +32,7 @@ public class TownDevelopmentSlider : MonoBehaviour
             Color color = GameManager.instance.Towns[i].GetComponent<TownBehaviour>().townColor;
             slidersImages[i].color = color;
             housesImages[i].color = color;
+            crownImages[i].SetActive(false);
         }
     }
     public void UpdateSliders(int townIndex, int score)
@@ -41,6 +43,31 @@ public class TownDevelopmentSlider : MonoBehaviour
         float previousValue = (float)scores[0] / totalScore;
         leftSlider.value = previousValue;
         middleSlider.value = previousValue + (float)scores[1] / totalScore;
+
+        UpdateCrown();
+    }
+
+    private void UpdateCrown()
+    {
+        if (scores[0] >= scores[1] && scores[0] >= scores[2])
+        {
+            crownImages[0].SetActive(true);
+            crownImages[1].SetActive(false);
+            crownImages[2].SetActive(false);
+        }
+        
+        else if (scores[1] >= scores[0] && scores[1] >= scores[2])
+        {
+            crownImages[0].SetActive(false);
+            crownImages[1].SetActive(true);
+            crownImages[2].SetActive(false);
+        }
+        else if (scores[2] >= scores[0] && scores[2] >= scores[1])
+        {
+            crownImages[0].SetActive(false);
+            crownImages[1].SetActive(false);
+            crownImages[2].SetActive(true);
+        }
     }
 
     public void MoveCameraToTown(int townIndex)
