bbf313d1d1b1566f9a67ab3e5003696242283588
Snibaw <Snibaw@gmail.com>
2024-01-29 16:57:10 +0100

    Pas de score mais un temps de survie

--- ZeusTest/Assets/Scripts/EndGamePanel.cs
+++ ZeusTest/Assets/Scripts/EndGamePanel.cs
@@ -11,8 +11,8 @@ public class EndGamePanel : MonoBehaviour
     
     public void ShowEndGamePanel(int currentScore)
     {
-        _bestScore.text = PlayerPrefs.GetInt("BestScore").ToString();
-        _currentScore.text = currentScore.ToString();
+        _bestScore.text = IntToTime(PlayerPrefs.GetInt("BestScore"));
+        _currentScore.text = IntToTime(currentScore);
     }
     public void RestartGame()
     {
@@ -21,5 +21,15 @@ public class EndGamePanel : MonoBehaviour
     public void GoToMainMenu()
     {
         SceneManager.LoadScene("MainMenu");
+    }
+    private string IntToTime(int time)
+    {
+        int minutes = time / 60;
+        int seconds = time % 60;
+        string minutesString = minutes.ToString();
+        string secondsString = seconds.ToString();
+        if (minutes < 10) minutesString = "0" + minutesString;
+        if (seconds < 10) secondsString = "0" + secondsString;
+        return minutesString + ":" + secondsString;
     }
 }
