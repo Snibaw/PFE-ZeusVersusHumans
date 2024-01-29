7d06369f0125cf19f19a3d1b8cf98cc890ff8336
Snibaw <Snibaw@gmail.com>
2024-01-29 15:49:13 +0100

    Explosion after hit lightning

--- ZeusTest/Assets/Scripts/LightningBehaviour.cs
+++ ZeusTest/Assets/Scripts/LightningBehaviour.cs
@@ -8,6 +8,7 @@ public class LightningBehaviour : MonoBehaviour
     public QuadraticCurve curve;
     public float speed = 0.1f;
     [SerializeField] private float speedAugmentationOverTime = 0.01f;
+    [SerializeField] private GameObject lightningImpactPrefab;
     private float sampleTime = 0;
     private float intensity = 1;
     private float magnitudeMultiplier = 3, roughness = 4, fadeInTime = 0.2f, fadeOutTime = 0.5f;
@@ -26,8 +27,9 @@ public class LightningBehaviour : MonoBehaviour
 
         if (sampleTime > 1)
         {
+            LightningImpact();
             Destroy(curve.gameObject);
-            Destroy(gameObject);
+            Destroy(gameObject,0.2f);
         }
     }
     
@@ -40,11 +42,13 @@ public class LightningBehaviour : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        LightningImpact();
+    }
+
+    private void LightningImpact()
+    {
         //Create a circle around the lightning depending on the intensity
         //If the circle collides with an object with the CanBeDestroyed Layer, access the script and do damage
-
-        bool needToBeDestroyed = (other.CompareTag("Ground") || other.CompareTag("Water"));
-        
         Collider[] colliders = Physics.OverlapSphere(transform.position, 2*intensity);
         foreach(Collider collider in colliders)
         {
@@ -56,21 +60,28 @@ public class LightningBehaviour : MonoBehaviour
                     continue;
                 }
                 collider.GetComponent<ObjectToDestroy>().TakeDamage(Mathf.Clamp01(intensity)*100, true);
-                needToBeDestroyed = true;
             }
         }
 
-
-        if (needToBeDestroyed)
+        Vibrator.Vibrate((long)(300 * intensity));
+        CameraShaker.Instance.ShakeOnce(intensity * magnitudeMultiplier, roughness, fadeInTime, fadeOutTime);
+        AudioManager.instance.PlaySoundEffect(SoundEffects.LightningImpact, Mathf.Clamp01(intensity));
+        
+        RaycastHit hit;
+        GameObject impact;
+        if (Physics.Raycast(transform.position, -transform.position, out hit, 100f, LayerMask.GetMask("Default")))
         {
-            Vibrator.Vibrate((long)(300 * intensity));
-            CameraShaker.Instance.ShakeOnce(intensity * magnitudeMultiplier, roughness, fadeInTime, fadeOutTime);
-            AudioManager.instance.PlaySoundEffect(SoundEffects.LightningImpact, Mathf.Clamp01(intensity));
-            Destroy(curve.gameObject);
-            Destroy(gameObject);
+            Debug.Log("Ray hit " + hit.collider.gameObject.name);
+            impact = Instantiate(lightningImpactPrefab, hit.point, Quaternion.identity);
+            impact.transform.up =  - transform.position;
         }
-    }
-    private void OnDrawGizmos() {
-        Gizmos.DrawWireSphere(transform.position, intensity);
+        else
+        {
+            impact = Instantiate(lightningImpactPrefab, transform.position, Quaternion.identity);
+        }
+        impact.transform.localScale = intensity * Vector3.one;
+        Destroy(impact, 2f);
+        Destroy(curve.gameObject);
+        Destroy(gameObject);
     }
 }
